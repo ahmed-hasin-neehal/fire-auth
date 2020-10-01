06353bf0f51c6a973df1c62194860dc71e921857
ahmed-hasin-neehal <60386953+ahmed-hasin-neehal@users.noreply.github.com>
2020-10-01 13:22:31 +0600

    done with changes

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDvKv3h0nBbJI-_At8s4ZeRPtzREmhCRlo",
+    authDomain: "ema-john-simple-f6d57.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-f6d57.firebaseio.com",
+    projectId: "ema-john-simple-f6d57",
+    storageBucket: "ema-john-simple-f6d57.appspot.com",
+    messagingSenderId: "788416281860",
+    appId: "1:788416281860:web:e24319ebf61afa29bbe451"
+  };
+
+  export default firebaseConfig;
